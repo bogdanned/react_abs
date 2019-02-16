c990e95b65330d1a42e8cc9715000c1af83ece1c
bogdanned <bogdan.nedelcu@lokallead.de>
2019-02-16 19:09:45 +0100

    src

--- src/components/Assesment.js
+++ src/components/Assesment.js
@@ -1,93 +1,87 @@
 
-import React from "react"
+import React from 'react'
 
-import { version, Button, Progress, Radio } from 'antd';
-import RenderPain from "./RenderPain"
-import Question from "./Question"
-import RadioGroupFull from "../RadioGroup"
+import { version, Button, Progress, Radio } from 'antd'
+import RenderPain from './RenderPain'
+import Question from './Question'
+import RadioGroupFull from '../RadioGroup'
 
 import { Spin } from 'antd'
 
-import questionList from "./questionList"
+import questionList from './questionList'
 
-import voice from "../util/voice"
+import voice from '../util/voice'
 
 export default class Assesment extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            questionIndex: 0,
-            answers: {},
-            thinking: false
-        }
-        this.answerQuestion = this.answerQuestion.bind(this)
+  constructor (props) {
+    super(props)
+    this.state = {
+      questionIndex: 0,
+      answers: {},
+      thinking: false
+    }
+    this.answerQuestion = this.answerQuestion.bind(this)
+  }
+
+  answerQuestion (e) {
+    const self = this
+    e.preventDefault()
+    this.setState({
+      thinking: true,
+      answers: {
+        ...this.state.answers,
+        [e.target.name]: e.target.value
+
+      }
+    })
+
+    setTimeout(function () {
+      // console.log(questionList[this.state.questionIndex + 1].label, "to say")
+      const text = questionList[self.state.questionIndex + 1].label
+      window.responsiveVoice.speak('Nice to meet you Frank!')
+      // voice(questionList[this.state.questionIndex + 1].label)
+      self.setState({
+        thinking: false,
+        questionIndex: self.state.questionIndex + 1
+      })
+    }, 2000)
     }
 
-    answerQuestion(e) {
-        const self = this
-        e.preventDefault()
-        this.setState({
-            thinking: true,
-            answers: {
-                ...this.state.answers,
-                [e.target.name]: e.target.value
+  render () {
+    const { thinking } = this.state
+    const question = questionList[this.state.questionIndex]
+    const { label, options, name } = question
+    const progressValue = (this.state.questionIndex + 1) / questionList.length * 100
+    // console.log(progressValue , "progressValue ")
+    return (
+      <div className="assesmentInner">
+        <div className="renderPainContainer">
+          <RenderPain />
+        </div>
+        <div className="aiContainer">
 
-            },
-        })
+          <p>AVA</p>
 
-        setTimeout(function () {
-            // console.log(questionList[this.state.questionIndex + 1].label, "to say")
-            const text = questionList[self.state.questionIndex + 1].label
-            window.responsiveVoice.speak('Nice to meet you Frank!')
-            //voice(questionList[this.state.questionIndex + 1].label)
-            self.setState({
-                thinking: false,
-                questionIndex: self.state.questionIndex + 1
-            })
-        }, 2000);
-    }
+          <Progress percent={progressValue} />
 
+          <Question
+            label={label}
+            options={options}
+            thinking={thinking}
+            name={name}
+            thinking={thinking}
+            onChange={this.answerQuestion}
+          />
 
-
-
-    render() {
-        const { thinking } = this.state
-        const question = questionList[this.state.questionIndex]
-        const { label, options, name } = question
-        const progressValue = (this.state.questionIndex + 1) / questionList.length * 100
-        // console.log(progressValue , "progressValue ")
-        return (
-            <div className="assesmentInner">
-                <div className="renderPainContainer">
-                    <RenderPain />
-                </div>
-                <div className="aiContainer">
-
-                    <p>AVA</p>
-
-                    <Progress percent={progressValue} />
-
-                    <Question
-                        label={label}
-                        options={options}
-                        thinking={thinking}
-                        name={name}
-                        thinking={thinking}
-                        onChange={this.answerQuestion}
-                    />
-
-                    {this.state.thinking ? 
-                    <div className="spinnerContainer">
-                        <Spin size="large" />
-                    </div>
-                     : null}
-
-
-
-                </div>
-
+          {this.state.thinking 
+                    ? <div className="spinnerContainer">
+              <Spin size="large" />
             </div>
-        )
-    }
+            : null}
 
+        </div>
+
+      </div>
+    )
+  }
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,48 +1,46 @@
 
-import React from "react"
-import { Radio, Input } from 'antd';
+import React from 'react'
+import { Radio } from 'antd'
 
-const RadioGroup = Radio.Group;
-const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group
+const RadioButton = Radio.Button
 
 const radioStyle = (thinking) => ({
-    display: !thinking ? 'block' : 'none',
-    height: '30px',
-    lineHeight: '25px',
-    borderLeft: 1,
-    margin: '5px'
+  display: !thinking ? 'block' : 'none',
+  height: '30px',
+  lineHeight: '25px',
+  borderLeft: 1,
+  margin: '5px'
 })
 
-
 const groupStyle = {
-    textAlig: "left"
+  textAlig: 'left'
 }
 
 const renderOptions = (options, thinking) => {
-    return options.map((opt,index) => (
-        <RadioButton className="option" key={index} style={radioStyle(thinking)} value={opt.value}>{opt.label}</RadioButton >
-    ))
+  return options.map((opt, index) => (
+    <RadioButton className="option" key={index} style={radioStyle(thinking)} value={opt.value}>{opt.label}</RadioButton >
+  ))
 }
 
-export default class Question extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            value: ""
-        }
+export default class Question extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: ''
     }
+  }
 
-    render(){
-        const {label, options, onChange, name, thinking} = this.props
-        const renderedOptions = renderOptions(options, thinking)
-        return (
-            <div>
-                <h3>{label}</h3>
-                <RadioGroup styles={groupStyle} onChange={onChange} value={this.state.value} name={name}>
-                    {renderedOptions}
-                </RadioGroup>
-            </div>
-        )
-    }
-
+  render () {
+    const { label, options, onChange, name, thinking } = this.props
+    const renderedOptions = renderOptions(options, thinking)
+    return (
+      <div>
+        <h3>{label}</h3>
+        <RadioGroup styles={groupStyle} onChange={onChange} value={this.state.value} name={name}>
+          {renderedOptions}
+        </RadioGroup>
+      </div>
+    )
+  }
 }
