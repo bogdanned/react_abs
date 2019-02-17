3638a50066727b77e502bb256b8b167fe8e2214f
bogdanned <bogdan.nedelcu@lokallead.de>
2019-02-17 10:10:13 +0100

    assesments

--- src/components/Assesment.js
+++ src/components/Assesment.js
@@ -13,92 +13,111 @@ import questionList from './questionList'
 import voice from '../util/voice'
 
 export default class Assesment extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      questionIndex: 0,
-      answers: {},
-      thinking: false
-    }
-    this.answerQuestion = this.answerQuestion.bind(this)
-    this.submit = this.submit.bind(this)
-  }
-
-  submit(data){
-    console.log(data, "data")
-    const self = this
-    this.setState({
-        thinking: true,
-        answers: {
-          ...this.state.answers,
-          ...data
-  
+    constructor(props) {
+        super(props)
+        this.state = {
+            questionIndex: 0,
+            answers: {},
+            displayReport: false,
+            thinking: false
         }
-    })
-    console.log(this.state)
-  }
-
-  answerQuestion (e) {
-    const self = this
-    e.preventDefault()
-    this.setState({
-      thinking: true,
-      answers: {
-        ...this.state.answers,
-        [e.target.name]: e.target.value
-
-      }
-    })
-
-    setTimeout(function () {
-      // console.log(questionList[this.state.questionIndex + 1].label, "to say")
-      const text = questionList[self.state.questionIndex].machine_answer
-      text && window.responsiveVoice.speak(text)
-      self.setState({
-        thinking: false,
-        questionIndex: self.state.questionIndex + 1
-      })
-      text && window.responsiveVoice.speak(questionList[self.state.questionIndex].label)
-    }, 2000)
+        this.answerQuestion = this.answerQuestion.bind(this)
+        this.submit = this.submit.bind(this)
     }
 
-  render () {
-    const { thinking } = this.state
-    const question = questionList[this.state.questionIndex]
-    const { label, options, name, textType } = question
-    const progressValue = (this.state.questionIndex + 1) / questionList.length * 100
-    // console.log(progressValue , "progressValue ")
-    return (
-      <div className="assesmentInner">
-        <div className="renderPainContainer">
-          <RenderPain />
-        </div>
-        <div className="aiContainer">
+    submit(data) {
+        console.log(data, "data")
+        const self = this
+        this.setState({
+            thinking: true,
+            answers: {
+                ...this.state.answers,
+                ...data
 
-          <p>AVA</p>
+            }
+        })
+        console.log(this.state)
+    }
 
-          <Progress percent={progressValue} strokeWidth={15}/>
+    answerQuestion(e) {
+        e.preventDefault()
+        const self = this
+        this.setState({
+            thinking: true,
+            answers: {
+                ...this.state.answers,
+                [e.target.name]: e.target.value
+            }
+        })
+        console.log(self.state.questionIndex)
+        console.log(questionList.length, "question list length")
+        if (self.state.questionIndex < questionList.length - 1) {
+            setTimeout(function () {
+                // console.log(questionList[this.state.questionIndex + 1].label, "to say")
+                const text = questionList[self.state.questionIndex].machine_answer
+                text && window.responsiveVoice.speak(text)
+                self.setState({
+                    thinking: false,
+                    questionIndex: self.state.questionIndex + 1
+                })
+                text && window.responsiveVoice.speak(questionList[self.state.questionIndex].label)
+            }, 2000)
+        } else {
+            this.setState({
+                displayReport: true
+            })
+        }
 
-          <Question
-            label={label}
-            options={options}
-            thinking={thinking}
-            name={name}
-            textType={textType}
-            thinking={thinking}
-            submit={this.submit}
-            onChange={this.answerQuestion}
-          />
 
-          {this.state.thinking 
-                    ? <div className="spinnerContainer">
-              <Spin size="large" />
+    }
+
+
+
+    render() {
+        const { thinking } = this.state
+        const question = questionList[this.state.questionIndex]
+        const { label, options, name, textType } = question
+        const progressValue = (this.state.questionIndex + 1) / questionList.length * 100
+        // console.log(progressValue , "progressValue ")
+        return (
+            <div className="assesmentInner">
+                <div className="renderPainContainer">
+                    <RenderPain />
+                </div>
+                <div className="aiContainer">
+
+                    <p>AVA</p>
+
+                    {!this.state.displayReport ?
+                        <div>
+                            <Progress percent={progressValue} strokeWidth={15} />
+
+                            <Question
+                                label={label}
+                                options={options}
+                                thinking={thinking}
+                                name={name}
+                                textType={textType}
+                                thinking={thinking}
+                                submit={this.submit}
+                                onChange={this.answerQuestion}
+                            />
+
+                            {this.state.thinking
+                                ? <div className="spinnerContainer">
+                                    <Spin size="large" />
+                                </div>
+                                : null}
+                        </div>
+                        :
+                        <div>
+                            Download report
+                        </div>
+                    }
+
+                </div>
+
             </div>
-            : null}
-
-        </div>
-
-      </div>
-    )
-  }
+        )
+    }
 }
