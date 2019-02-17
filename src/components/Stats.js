4d673c212b97c9768ddc9037b8cbae5aa7090ef2
Moritz Pfennig <moritz@Moritzs-MacBook-Pro.local>
2019-02-17 13:10:22 +0100

    push

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -23,9 +23,9 @@ export default () => {
                     <Tag style={{margin: "5px"}} color="#00e600">Low abdominal pain</Tag>
                     <Tag color="#00cc00">Pain up to one week</Tag>
                     <Tag style={{margin: "5px"}} color="#00b300">Dull ache</Tag>
-                    <Tag color="#009900">Moderat pain intensity</Tag>
+                    <Tag color="#009900">Moderate pain intensity</Tag>
                     <Tag style={{margin: "5px"}}color="#008000">Bloating</Tag>
-                    <Tag color="#006600">hard stool</Tag>
+                    
                 </div>
                 <Row gutter={16}>
                     <Col span={12}>
@@ -54,11 +54,9 @@ export default () => {
                     </Col>
                 </Row>
             </div>
-            <Popover content={content}>
                 <a href="/report.pdf" target="_blank">
-                    <Button type="primary" className="buttonRep">Download Report</Button>
+                    <Button type="primary" className="buttonRep">Download Symptom Report</Button>
                 </a>
-            </Popover>
         </div>
     )
 }
